224c1fe2555da417411826cec9dbd29520767def
GustavoMS97 <gustavo@tfcsolutions.tech>
2025-04-03 14:40:09 -0300

    feat: add new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="Notes" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className="h-screen overflow-hidden flex flex-col">
         <AuthProvider>{children}</AuthProvider>
       </body>
